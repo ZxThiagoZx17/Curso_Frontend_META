9e4a0ae3df7440d487ae88fe76c8ff901ced5368
unknown <tiagosanabria03@gmail.com>
2024-08-28 20:02:01 -0500

    Se crea script donde se muestra declaracion de variables

---
+++ Programar en JavaScript/Variables.js
@@ -0,0 +1,7 @@
+var persona = 'Santiago';  //Declaramos la variable persona
+console.log(persona);
+
+console.log("Hola",persona);//Concatenacion con ,
+
+console.log("Hola"+persona);//Concatenacion con +
+
