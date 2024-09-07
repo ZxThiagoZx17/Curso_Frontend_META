d6510e8d29838e7e29109317a03d31ea39d5f1dd
unknown <tiagosanabria03@gmail.com>
2024-09-07 18:23:04 -0500

    Se añade 8vo laboratorio en introduccion a React

---
+++ Introduccion a React/Laboratorios/8vo laboratorio/Files/home/coder/project/reactlab/src/App.js
@@ -0,0 +1,22 @@
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Task: Add an image below</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import logo from"./assets/logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task: Add an image below</h1>
+      <img src={logo} alt="Logo" />
+    </div>
+  );
+};
+
+export default App;
