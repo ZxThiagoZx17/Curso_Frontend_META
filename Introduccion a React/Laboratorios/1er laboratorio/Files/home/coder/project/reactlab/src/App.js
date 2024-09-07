0ef8c80068fc6be4d8538c1278ac7cdd23c85cf8
unknown <tiagosanabria03@gmail.com>
2024-09-07 10:37:14 -0500

    Se añade 1er laboratorio en introduccion a React

---
+++ Introduccion a React/Laboratorios/1er laboratorio/Files/home/coder/project/reactlab/src/App.js
@@ -0,0 +1,27 @@
+function App() { 
+  return ( 
+    <div className="App"> 
+      This is the starting code for "Your first component" ungraded lab 
+      <Heading /> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
+
+function Heading() { 
+  return ( 
+    <h1>Este es un H1.</h1> 
+  ) 
+} 
+ 
+function App() { 
+  return ( 
+    <div className="App"> 
+      This is the starting code for "Your first component" ungraded lab 
+      <Heading /> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
