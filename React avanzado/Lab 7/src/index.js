84c83c8963a8a94a8c1795bc7badda50ca512231
unknown <tiagosanabria03@gmail.com>
2024-09-08 15:44:32 -0500

    Se añade 7mo laboratorio en curso de react avanzado

---
+++ React avanzado/Lab 7/src/index.js
@@ -0,0 +1,48 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import * as React from "react";
+
+export const RadioGroup = ({ onChange, selected, children }) => { 
+ const RadioOptions = React.Children.map(children, (child) => { 
+   return React.cloneElement(child, { 
+     onChange, 
+     checked: child.props.value === selected, 
+   }); 
+ }); 
+ return <div className="RadioGroup">{RadioOptions}</div>; 
+}; 
+ 
+export const RadioOption = ({ value, checked, onChange, children }) => { 
+ return ( 
+   <div className="RadioOption"> 
+     <input 
+       id={value} 
+       type="radio" 
+       name={value} 
+       value={value} 
+       checked={checked} 
+       onChange={(e) => { 
+         onChange(e.target.value); 
+       }} 
+     /> 
+     <label htmlFor={value}>{children}</label> 
+   </div> 
+ ); 
+}; 
