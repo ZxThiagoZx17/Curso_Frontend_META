7b806d7d1c80046df5bb837deae9c06b3bc1cc1f
unknown <tiagosanabria03@gmail.com>
2024-09-08 08:58:30 -0500

    Se añade 1er laboratorio en curso de react avanzado

---
+++ React avanzado/Lab 1/src/App.js
@@ -0,0 +1,51 @@
+import "./App.css";
+import DessertsList from "./DessertsList";
+
+const desserts = [
+  {
+    name: "Chocolate Cake",
+    calories: 400,
+    createdAt: "2022-09-01",
+  },
+  {
+    name: "Ice Cream",
+    calories: 200,
+    createdAt: "2022-01-02",
+  },
+  {
+    name: "Tiramisu",
+    calories: 300,
+    createdAt: "2021-10-03",
+  },
+  {
+    name: "Cheesecake",
+    calories: 600,
+    createdAt: "2022-01-04",
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <h2>List of low calorie desserts:</h2>
+      <DessertsList data={desserts} />
+    </div>
+  );
+}
+
+const lowCaloriesDesserts = props.data 
+ .filter((dessert) => { 
+   return dessert.calories < 500; 
+ }) 
+ .sort((a, b) => { 
+   return a.calories - b.calories; 
+ }) 
+ .map((dessert) => { 
+   return ( 
+     <li> 
+       {dessert.name} - {dessert.calories} cal 
+     </li> 
+   ); 
+ });
+ 
+export default App;
---
+++ React avanzado/Laboratorios/1er laboratorio/Files/home/coder/project/reactlab/src/DessertsList.js
@@ -0,0 +1,26 @@
+// function DessertsList(props) {
+//   // Implement the component here.
+//   return null;
+// }
+
+// export default DessertsList;
+const DessertsList = (props) => {
+ const lowCaloriesDesserts = props.data
+   .filter((dessert) => {
+     return dessert.calories < 500;
+   })
+   .sort((a, b) => { 
+     return a.calories - b.calories; 
+   })
+   .map((dessert) => { 
+     return ( 
+       <li>
+         {dessert.name} - {dessert.calories} cal 
+       </li> 
+     ); 
+   }); 
+ return <ul>{lowCaloriesDesserts}</ul>; 
+
+}
+export default DessertsList; 
+
