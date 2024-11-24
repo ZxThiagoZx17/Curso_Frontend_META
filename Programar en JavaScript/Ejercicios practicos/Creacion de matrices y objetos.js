865a411d799a70f703c827755b2e224df7bcd161
unknown <tiagosanabria03@gmail.com>
2024-11-24 15:22:09 -0500

    Se añde ejercicio practico sobre matrices y objetos

---
+++ Programar en JavaScript/Ejercicios practicos/Creacion de matrices y objetos.js
@@ -0,0 +1,21 @@
+var ropa = [];
+
+ropa.push("Pantalones cargo");
+ropa.push("Camisa elegante");
+ropa.push("Zapatos Nike");
+ropa.push("Gorra W");
+ropa.push("Bolso LV");
+
+console.log(ropa);
+
+ropa.pop();
+ropa.push("Bolso Cartier");
+
+console.log(ropa[2]);
+
+var favCar = {};
+
+favCar.color = "Azul";
+favCar.convertible = true;
+
+console.log (favCar)
