080a9078fa626cb2514e3b18cce133d537fbefc3
unknown <tiagosanabria03@gmail.com>
2024-12-28 11:04:12 -0500

    Se termina la explicacion sobre la desestructuracion de matrices y objetos

--- Programar en JavaScript/Desestructuracion_matrices_objetos.js
+++ Programar en JavaScript/Desestructuracion_matrices_objetos.js
@@ -1,2 +1,7 @@
-let {PI} = Math;
-PI;
+let {PI} = Math; //Traemos Math.PI de esta manera, siedo Math la libreria de donde vamos a traer la constante o valor
+console.log(PI)
+
+let {pi} = Math; //Intentamos traer el numero PI pero no funciona debido a como lo escribimos, nos arrojara indefinido
+console.log(pi)
+
+console.log(PI === Math.PI)
