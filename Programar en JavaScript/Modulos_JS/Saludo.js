05b99fe5d5185b59e6e85d414a852f4de0ff0c78
unknown <tiagosanabria03@gmail.com>
2025-01-08 16:52:33 -0500

    Se muestran como funcionan los modulos y scripts en el navegador

---
+++ Programar en JavaScript/Modulos_JS/Saludo.js
@@ -0,0 +1,13 @@
+export const SaludoFormal = (persona) => {
+    console.log(`Buenas Noches ${persona}`);
+}
+
+export const SaludoNormal = (persona) => {
+    console.log(`Hola! ${persona}, que gusto verte!`);
+}
+
+const Saludo = () => {
+    console.log(`Que dice`);
+}
+
+export default Saludo;
