0807df1ea84c3d737d4ce93e7384db1de20db8c5
unknown <tiagosanabria03@gmail.com>
2025-01-12 22:07:51 -0500

    Se da una introduccion a pruebas en JS

---
+++ Programar en JavaScript/Pruebas/Tipos_Pruebas.js
@@ -0,0 +1,20 @@
+//Hay muchos marcos enfocado a la automatizacion de pruebas en JS
+
+//Hay varios tipos de pruebas en programacion
+
+// 1. e2e (Extremo a Extremo)
+// Se intenta imitar como un usuario usaria la aplicacion, algunos marcos de prueba serian estos
+// - WebdriverJS
+// - Protractor
+// - Cypress
+
+// 2.Integracion
+// Comprueban como nuestro codigo interactua con otras partes de nuestra aplicacion, marcos:
+// - React Testing Library
+// - Enzyme
+
+// 3.Unitaria
+// Prueba pequeñas partes de codigo por individual como por ejemplo, funciones
+
+// En proyectos grande se aconseja empezar con:
+// Unitaria ---> Integracion ---> e2e
