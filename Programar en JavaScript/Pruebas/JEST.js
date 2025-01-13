af006d9dd07223af67c1605d7666b5de33dfa535
unknown <tiagosanabria03@gmail.com>
2025-01-12 22:36:10 -0500

    Se añaden varios archivos, se instalan dependencias

---
+++ Programar en JavaScript/Pruebas/JEST.js
@@ -0,0 +1,19 @@
+// JS no tiene integrada ninguna funcion u objeto encargada de pruebas, se han creado muchas bibliotecas para abordar esto:
+
+//Jasmine, Mocha, Karma, qUnit y Jest
+
+// Jest es un marco de prueba en JS mantenido por Meta y otros desarrolladores, tambien compatible con tecnologias como:
+// React
+// TypeScript
+// Node 
+// Angular
+// Vue
+// Babel
+
+// La simulacion o Moking es importante a la hora de hacer pruebas cuando se esta en un equipo de trabajo
+
+function AddFive(val) {
+    return val+5;
+}
+
+// en Test de package.json Escribo "jest"
