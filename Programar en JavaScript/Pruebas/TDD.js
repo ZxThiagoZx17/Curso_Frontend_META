ede85507dffb4cfa23033b3ea3a835452eed6ec1
unknown <tiagosanabria03@gmail.com>
2025-01-13 15:35:17 -0500

    Se da una introcuccion a TDD Test Driven Development

---
+++ Programar en JavaScript/Pruebas/TDD.js
@@ -0,0 +1,7 @@
+// TDD Test-Driven Development (Desarrollo guiado por pruebas)
+// Es una metodologia de desarrollo de Software donde las pruebas se crean antes del codigo fuente
+
+// La idea es saber como se quiere que funcione el programa antes de codificarlo, sigue estos 3 pasos:
+// Red (Escribir una prueba que falle)
+// Green (Escribir una prueba que pase)
+// Refactor (Mejorar el codigo que se hizo, limpieza, optimizacion, legibilidad)
