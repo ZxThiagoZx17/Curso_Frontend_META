2bc7b57101e80650aa29cc66edc0dd3eba4e3c28
unknown <tiagosanabria03@gmail.com>
2025-02-11 10:21:15 -0500

    Se habla de integracion continua CI y de bibliotecas de UI

---
+++ React avanzado/Modulo 3/CI.js
@@ -0,0 +1,13 @@
+// Integración Continua (CI) es una técnica de desarrollo de software en la que los desarrolladores utilizan un sistema de control de versiones, como Git donde los desarrolladores diariamente hacen sus contribuciones al desarrollo
+
+// Cada cambio subido desencadena un conjunto de funciones que prueban y testean automaticamente el codigo, si alguno arroja un error, el CI se detiene hasta solucionarlo
+
+// Al tener pequeños cambios controlados es mas sencillo solucionar los errores, es un flujo de trabajo muy comun en organizaciones, seria asi:
+// Se crea la rama de codigo en GitHub
+// Cuando el desarrollador envia su codigo el CI lo emula y hace las pruebas automatizadas
+// En caso de salir mal, el desarrollador que lo subio le llegara un correo y el CI se pondra en rojo
+// De lo contrario si sale bien, estara en verde el software pasara a la siguiente etapa
+
+// Este flujo de trabajo nos trae muchas ventajas y automatiza varios procesos importantes en el proyecto 
+
+// Asi mismo es bueno seguir las guias de estilo propuestas por la organizacion para mejor comprension y documentacion del codigo
---
+++ React avanzado/Modulo 4/Proyecto final/BibliotecasUI.js
@@ -0,0 +1,41 @@
+// Hay varios bibliotecas en React para UI como ChakraUI, muy popular
+// Estas librerias nos proporcionan un conjunto de componentes prediseñados bien construidos, probados, etc... este agrupa varios componentes por categorias como diseño, formas, visualización de datos, retroalimentación, tipografía o superposición.
+
+// Estos componentes de diseño se encargan de delimitar el contenido, entre otras propiedades algunos componetes de diseño importantes son:
+// - HStack y VStack: Muestran a los elementos secundarios utilizando propiedades flexibles y elementos de pila vertical y horizontalmente
+// - Caja: Permite crear una caja con un color de fondo , borde, sombra, se necesita un background para establecer el color del fondo 
+
+// Tipografia (Diseño grafico de las letras), tenemos de ejemplo 2 componentes:
+// Encabezado: Representa una de las diferentes etiquetas de encabezado del DOM (h1, h2, h3…), Junto a la propiedad del tamaño para especificar el tamaño del encabezado:
+{/* <Heading as='h2' size='2xl'>
+    Little Lemon
+</Heading> */}
+
+// Texto, Se utiliza para representar texto y parrafo en un UI, ofrece una propiedad fontSize para aumentar el tamaño de fuente del texto:
+{/* <Stack>
+    <Text textStyle="xs">Chakra</Text>
+    <Text textStyle="sm">Chakra</Text>
+    <Text textStyle="md">Chakra</Text>
+    <Text textStyle="lg">Chakra</Text>
+    <Text textStyle="xl">Chakra</Text>
+    <Text textStyle="2xl">Chakra</Text>
+    <Text textStyle="3xl">Chakra</Text>
+    <Text textStyle="4xl">Chakra</Text>
+    <Text textStyle="5xl">Chakra</Text>
+    <Text textStyle="6xl">Chakra</Text>
+    <Text textStyle="7xl">Chakra</Text>
+</Stack> */}
+
+// En https://chakra-ui.com/docs/components/concepts/overview encontraremos toda la documentacion que necesitamos para trabajar, encontraremos informacion de varios componentes de interes, se nos mostrara como es graficamente y como se usa
+
+// Cabe racalcar que hay muchas bibliotecas de UI que nos facilitaran muchisimo la vida:
+// 1. PrimeReact
+// 2. Shadcn UI
+// 3. Material UI (MUI)
+// 4. Ant Design (AntD)
+// 5. React Bootstrap
+// 6. React Bootstrap
+
+// Tambien tenemos otros componentes externos como Formik y Yup la primera para crear formularios en React, tareas repetitivas de gestión del estado del formulario, validación y envío, para que pueda centrarse en la lógica de negocio 
+
+// Y yup sirve para validar datos del formulario antes de enviarlos al servidor 
