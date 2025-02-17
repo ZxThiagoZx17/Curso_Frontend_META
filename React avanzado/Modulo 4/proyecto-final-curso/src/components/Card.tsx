34c1fcf9792b27be514c83e2d667102df166ef45
unknown <tiagosanabria03@gmail.com>
2025-02-16 20:57:36 -0500

    Se modifican pequeños detalles

--- React avanzado/Modulo 4/proyecto-final-curso/src/components/Card.tsx
+++ React avanzado/Modulo 4/proyecto-final-curso/src/components/Card.tsx
@@ -1,9 +1,15 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Heading, Image, Text } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
+//Se tiene que volver a tipar porque las props no heredan el tipado porque es un nuevo objeto
+interface CardProps {
+  title: string;
+  description: string;
+  imageSrc: string;
+}
 
-const Card = ({ title, description, imageSrc }) => {
+const Card:React.FC<CardProps> = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
