0d0bac241c282dae586aeaa760787f350584c7d5
unknown <tiagosanabria03@gmail.com>
2025-02-18 08:57:57 -0500

    Se añaden varias extensiones utiles junto con sus configuraciones

---
+++ React avanzado/Modulo 4/ExtensionesRecomendadas.js
@@ -0,0 +1,23 @@
+// Dentro de Vs Code tenemos un gran numero de extensiones muy utiles para trabajar, se mostraran, se instalaran y se configuraran varias extensiones muy utiles para trabajar con TSX
+
+// _____________________________________________________________________
+// Programacion en general VSCode
+// _____________________________________________________________________
+// 1. ESLint: Detecta errores en TypeScript y sugiere mejoras en el codigo, avisa de errores en variables no usadas, problemas con imports y mas
+
+// Lo podemos instalar en las dependencias del desarrollo del build del proyecto (Package.json), asi ya viene todo configurado, esto se tiene que inicializar con una serie de comandos, etc... para definir como se trabajara el proyecto, es bueno ver videos de como configurarlo
+
+// 2. Prettier - Code formatter: Nos sirve para formatear el codigo, organizarlo mejor, poner ; donde haga falta, etc... se configura con la opcion "Dar formato al documento con..." se elige prettiery luego se da alt+shift+f para formatear el codigo
+
+// - Podemos configurarlo a nuestro gusto segun las reglas del proyecto, por ejemplo que en vez de usar 2 espacios se usen 4 para identar, etc... basicamente el code style
+
+// 3. Error Lens: Muestra los errores directamente en el código, sin necesidad de pasar el cursor por encima, util para detectar errores mas rapido
+
+// _____________________________________________________________________
+// Especificamente para React y TSX
+// _____________________________________________________________________
+// 1. React Developer Tools (Extensión de Chrome): Es esencial para debuggear componentes de React en el navegador.
+
+// 2. Tailwind CSS IntelliSense: Autocompleta clases de Tailwind y muestra errores si se escriben mal, resalta en colores las clases de Tailwind en el código.
+
+// 3. HTMLHint: Detecta errores en HTML, como IDs duplicados o etiquetas mal cerradas, util para trabajar TSX y JSX
--- React avanzado/Modulo 4/proyecto-final-curso/eslint.config.js
+++ React avanzado/Modulo 4/proyecto-final-curso/eslint.config.js
@@ -19,6 +19,10 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'indent': [
+        'error',
+        2
+      ],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
